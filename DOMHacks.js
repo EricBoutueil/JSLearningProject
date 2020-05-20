bdb9adca50f35e56bd974e04d9b5a5046236dfea
Eric Boutueil <e.boutueil@gmail.com>
2020-05-20 20:56:18 +0200

    feat: DOM Manipulation section structure + Hacks chapter

---
+++ DOMHacks.js
@@ -0,0 +1,21 @@
+let val;
+
+/***
+ * DRUNK
+ ***/
+addChapter("DOMHacks", "DRUNK");
+
+// var elements = document.querySelectorAll('div,p,span,img,a,body');
+// for(var i = 0; i < elements.length; i++){
+// 	elements[i].style['transform'] = 'rotate(' + (Math.floor(Math.random() * 3) - 1) + 'deg)';
+// }
+
+/***
+ * CLICK FLIP
+ ***/
+addChapter("DOMHacks", "CLICK FLIP");
+
+// document.onclick = function(){
+// 	document.body.style['transition'] = 'transform 3s';
+// 	document.body.style['transform'] = 'rotate(180deg)';
+// }
